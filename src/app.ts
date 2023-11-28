50ca266e8d377b9f91614e8783d3ebcd63c83ae8
rakibul12212 <rakibul15-12212@diu.edu.bd>
2023-11-28 11:53:09 -0800

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
- import express, { Application, Request, Response } from "express"
-import cors from "cors"
- const app:Application = express()
-
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req:Request, res:Response) => {
-    const a=10
-  res.send(a)
-})
+app.get("/", (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
 
 console.log(process.cwd());
 
